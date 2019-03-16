c0cd1f13d21e4a3eba8f32b223a31d916f50a94a
Joost van der Griendt <joostvdg@gmail.com>
2019-03-16 17:50:43 +0100

    differentiate between local build and CI build locally we would want to build with buildkit, CI build likely to be Kaniko

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-docker build -t sunrise --progress plain .
+## This is for building with BuildKit!
+echo "This will only work with BuildKit!"
+docker build -t sunrise --progress plain -f Dockerfile.buildkit .
 echo
 echo
 echo "To run the docker container execute:"
